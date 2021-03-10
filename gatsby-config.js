2173b1338216206a04507090826a97b4a58b0506
arturoacevedob@gmail.com <arturoacevedob@gmail.com>
2021-03-10 12:19:46 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteMetadata: {
+    title: "neue motion bookmarks",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "uqgclw23",
+        dataset: "",
+      },
+    },
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "263468480",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
